aff50af710931bbe82708452500bc843ce4bf6e2
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2015-09-11 10:12:15 -0430

    Renamed README file

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.2dev'
 setup(name='collective.seo',
       version=version,
       description="SEO add-on let you set values of some html header tags.",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
